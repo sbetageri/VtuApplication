c447f50e4c01a436bdb1105d8a1304f6b5db5f50
sbetageri111 <sbetageri111@gmail.com>
2016-08-07 23:07:39 +0530

    Integrate all components

--- src/main/java/com/example/admin/vtuapplicatoin/MainActivity.java
+++ src/main/java/com/example/admin/vtuapplicatoin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.vtuapplicatoin;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String AUTHORITY = "com.example.admin.vtuapplicatoin.provider";
 
-    public static final String ACCOUNT_TYPE = "results.vtu.ac.in";
+    public static final String ACCOUNT_TYPE = "sai.ram";
 
-    public static final String ACCOUNT = "an_account";
+    public static final String ACCOUNT = "account";
+
+    public static final long SYNC_INTERVAL = 9L;
+
+    public ContentResolver mResolver;
 
     public Account mAccount;
             /*
@@ -27,16 +32,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAccount = createSyncAccount(this);
+        mResolver = getContentResolver();
+        ContentResolver.addPeriodicSync(mAccount,
+                AUTHORITY,
+                Bundle.EMPTY,
+                SYNC_INTERVAL);
+        Log.e(_TAG, "after periodic sync");
     }
 
-    public static Account createSyncAccount(Context context) {
-        Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
-        AccountManager am = (AccountManager)context.getSystemService(ACCOUNT_SERVICE);
+    public Account createSyncAccount(Context context) {
+        String accType = getResources().getString(R.string.account_type_str);
 
-        if(am.addAccountExplicitly(newAccount, null, null)) {
+        Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+        Log.e(_TAG, "type: " + newAccount.type);
+        Log.e(_TAG, "type: " + accType);
+        AccountManager am = (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+
+        if (am.addAccountExplicitly(newAccount, null, null)) {
             Log.e(_TAG, "added account explicitly");
         } else {
-            Log.e(_TAG, "some error");
+            Log.e(_TAG, "account exists");
         }
         return newAccount;
     }
